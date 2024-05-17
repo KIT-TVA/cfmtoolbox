4f09f53eb8f31f98be1bbe55c053c9c5e8b7ebc9
Jonathan Ehwald <github@ehwald.info>
2024-05-17 20:30:02 +0200

    Add basic plugin loading capabilities

--- cfmtoolbox/cfmtoolbox/__main__.py
+++ cfmtoolbox/cfmtoolbox/__main__.py
@@ -1,1 +1,8 @@
+from importlib.metadata import entry_points
+
+plugin_entry_points = entry_points(group="cfmtoolbox.plugins")
+plugins = {ep.name: ep.load() for ep in plugin_entry_points}
+
 print("Hello, cfmtoolbox!")
+print("Discovered plugins:", plugin_entry_points)
+print("Loaded plugins:", plugins)
