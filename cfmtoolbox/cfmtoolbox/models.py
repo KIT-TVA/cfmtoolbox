2165174acdec4d4012b0b31500ff23c5ebada806
Anna Chen <chenxanna@gmail.com>
2024-05-18 23:27:10 +0200

    Add basic datastructure

--- cfmtoolbox/cfmtoolbox/models.py
+++ cfmtoolbox/cfmtoolbox/models.py
@@ -1,2 +1,51 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Interval:
+    lower: int | None
+    upper: int | None
+
+    def __str__(self) -> str:
+        lower_formatted = "*" if self.lower is None else self.lower
+        upper_formatted = "*" if self.upper is None else self.upper
+        return f"{lower_formatted}..{upper_formatted}"
+
+
+@dataclass
+class Cardinality:
+    intervals: list[Interval]
+
+    def __str__(self) -> str:
+        return ", ".join(map(str, self.intervals))
+
+
+@dataclass
+class Feature:
+    name: str
+    instance_cardinality: Cardinality
+    group_type_cardinality: Cardinality
+    group_instance_cardinality: Cardinality
+    parents: list["Feature"]
+    children: list["Feature"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+@dataclass
+class Constraint:
+    require: bool
+    first_feature: Feature
+    first_cardinality: Cardinality
+    second_feature: Feature
+    second_cardinality: Cardinality
+
+    def __str__(self) -> str:
+        return f"{self.first_feature.name} -> {self.second_feature.name}"
+
+
 class CFM:
-    pass
+    features: list[Feature]
+    require_constraints: list[Constraint]
+    exclude_constraints: list[Constraint]
---
+++ tests/cfmtoolbox/test_models.py
@@ -0,0 +1,39 @@
+import pytest
+
+from cfmtoolbox.models import Cardinality, Constraint, Feature, Interval
+
+
+@pytest.mark.parametrize(
+    ["lower", "upper", "expectation"],
+    [(1, 10, "1..10"), (None, 2, "*..2"), (2, None, "2..*"), (None, None, "*..*")],
+)
+def test_interval_string(lower, upper, expectation):
+    interval = Interval(lower, upper)
+    assert str(interval) == expectation
+
+
+@pytest.mark.parametrize(
+    ["intervals", "expectation"],
+    [
+        ([], ""),
+        ([Interval(1, 10)], "1..10"),
+        ([Interval(1, 4), Interval(6, 10), Interval(24, 42)], "1..4, 6..10, 24..42"),
+    ],
+)
+def test_cardinality_string(intervals, expectation):
+    cardinality = Cardinality(intervals)
+    assert str(cardinality) == expectation
+
+
+def test_feature_string():
+    feature = Feature(
+        "Cheese", Cardinality([]), Cardinality([]), Cardinality([]), [], []
+    )
+    assert str(feature) == "Cheese"
+
+
+def test_constraint_string():
+    cardinality = Cardinality([])
+    feature = Feature("Cheese", cardinality, cardinality, cardinality, [], [])
+    constraint = Constraint(True, feature, cardinality, feature, cardinality)
+    assert str(constraint) == "Cheese -> Cheese"
