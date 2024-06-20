fda848965ba0a7d9fcad554566b233ae0a34eb45
Valle12 <valentinkaczmarek@yahoo.de>
2024-06-21 01:26:09 +0200

    Start of the import with the given python package

--- cfmtoolbox/plugins/uvl_import.py
+++ cfmtoolbox/plugins/uvl_import.py
@@ -1,7 +1,14 @@
+import antlr4
+from uvl import UVLPythonLexer, UVLPythonParser
+
 from cfmtoolbox import CFM, app
 
 
 @app.importer(".uvl")
 def import_uvl(data: bytes):
-    print("Importing UVL")
+    input_stream: antlr4.InputStream = antlr4.InputStream(data.decode("utf-8"))
+    lexer: UVLPythonLexer = UVLPythonLexer.UVLPythonLexer(input_stream)
+    token_stream: antlr4.CommonTokenStream = antlr4.CommonTokenStream(lexer)
+    parser: UVLPythonParser = UVLPythonParser.UVLPythonParser(token_stream)
+    print(parser.value())
     return CFM([], [], [])
