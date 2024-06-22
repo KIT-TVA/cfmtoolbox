c3d682e603d182d159ed4ae0a5cb09c3d31f324a
Valle12 <valentinkaczmarek@yahoo.de>
2024-06-22 19:45:42 +0200

    No errors, but values still don't get extracted

--- cfmtoolbox/plugins/uvl_import.py
+++ cfmtoolbox/plugins/uvl_import.py
@@ -1,5 +1,5 @@
 import antlr4
-from uvl import UVLPythonLexer, UVLPythonParser
+from uvl import UVLCustomLexer, UVLPythonParser
 
 from cfmtoolbox import CFM, app
 
@@ -7,7 +7,7 @@ from cfmtoolbox import CFM, app
 @app.importer(".uvl")
 def import_uvl(data: bytes):
     input_stream: antlr4.InputStream = antlr4.InputStream(data.decode("utf-8"))
-    lexer: UVLPythonLexer = UVLPythonLexer.UVLPythonLexer(input_stream)
+    lexer: UVLCustomLexer = UVLCustomLexer.UVLCustomLexer(input_stream)
     token_stream: antlr4.CommonTokenStream = antlr4.CommonTokenStream(lexer)
     parser: UVLPythonParser = UVLPythonParser.UVLPythonParser(token_stream)
     print(parser.value())
