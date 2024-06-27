33708d15f3bd10881c866dcc465a339a836bb338
Anna Chen <chenxanna@gmail.com>
2024-06-28 00:56:50 +0200

    Switch to more suitable data type

--- cfmtoolbox/plugins/featureide_import.py
+++ cfmtoolbox/plugins/featureide_import.py
@@ -1,10 +1,16 @@
+from enum import Enum
 from xml.etree import ElementTree as ET
 from xml.etree.ElementTree import Element
 
 from cfmtoolbox import app
 from cfmtoolbox.models import CFM, Cardinality, Feature, Interval
 
-node_choices = {"AND": "and", "OR": "or", "ALT": "alt", "FEATURE": "feature"}
+
+class Node_Types(Enum):
+    AND = "and"
+    OR = "or"
+    ALT = "alt"
+    FEATURE = "feature"
 
 
 def parse_instance_cardinality(is_mandatory: bool) -> Cardinality:
@@ -18,22 +24,22 @@ def parse_group_cardinality(feature: Element) -> Cardinality:
     lower = 0
     upper = 0
 
-    if feature.tag == node_choices["AND"] or feature.tag == node_choices["OR"]:
+    if feature.tag == Node_Types.AND.value or feature.tag == Node_Types.OR.value:
         upper = len(feature)
 
         for child in feature:
             if "mandatory" in child.attrib:
                 lower += 1
 
-        if feature.tag == node_choices["OR"]:
+        if feature.tag == Node_Types.OR.value:
             if lower == 0:
                 lower = 1
 
-    elif feature.tag == node_choices["ALT"]:
+    elif feature.tag == Node_Types.ALT.value:
         lower = 1
         upper = 1
 
-    elif feature.tag == node_choices["FEATURE"]:
+    elif feature.tag == Node_Types.FEATURE.value:
         lower = 0
         upper = 0
 
