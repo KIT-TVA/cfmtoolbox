9ffea1cdd091990bd23a3e6bca6ae231e8bd9a30
Jonathan Ehwald <github@ehwald.info>
2024-09-09 18:07:41 +0200

    Fix json impex could not handle circular refs (#18)

--- cfmtoolbox/plugins/json_export.py
+++ cfmtoolbox/plugins/json_export.py
@@ -1,9 +1,55 @@
 import json
-from dataclasses import asdict
 
-from cfmtoolbox import CFM, app
+from cfmtoolbox import CFM, Cardinality, Constraint, Feature, Interval, app
 
 
 @app.exporter(".json")
 def export_json(cfm: CFM) -> bytes:
-    return json.dumps(asdict(cfm), indent=2).encode()
+    root_feature = cfm.features[0]
+
+    serialized_root = serialize_feature(root_feature)
+    serialized_constraints = list(
+        map(serialize_constraint, [*cfm.require_constraints, *cfm.exclude_constraints])
+    )
+
+    serialized_cfm = {
+        "root": serialized_root,
+        "constraints": serialized_constraints,
+    }
+
+    return json.dumps(serialized_cfm, indent=2).encode()
+
+
+def serialize_feature(feature: Feature) -> dict:
+    return {
+        "name": feature.name,
+        "instance_cardinality": serialize_cardinality(feature.instance_cardinality),
+        "group_type_cardinality": serialize_cardinality(feature.group_type_cardinality),
+        "group_instance_cardinality": serialize_cardinality(
+            feature.group_instance_cardinality
+        ),
+        "children": list(map(serialize_feature, feature.children)),
+    }
+
+
+def serialize_cardinality(cardinality: Cardinality) -> dict:
+    return {
+        "intervals": list(map(serialize_interval, cardinality.intervals)),
+    }
+
+
+def serialize_interval(interval: Interval) -> dict:
+    return {
+        "lower": interval.lower,
+        "upper": interval.upper,
+    }
+
+
+def serialize_constraint(constraint: Constraint) -> dict:
+    return {
+        "require": constraint.require,
+        "first_feature_name": constraint.first_feature.name,
+        "first_cardinality": serialize_cardinality(constraint.first_cardinality),
+        "second_feature_name": constraint.second_feature.name,
+        "second_cardinality": serialize_cardinality(constraint.second_cardinality),
+    }
--- tests/plugins/test_json_export.py
+++ tests/plugins/test_json_export.py
@@ -1,20 +1,176 @@
 import json
 
-import cfmtoolbox.plugins.json_export as json_export_plugin
-from cfmtoolbox import CFM
-from cfmtoolbox.plugins.json_export import export_json
+from cfmtoolbox import CFM, Cardinality, Constraint, Feature, Interval
+from cfmtoolbox.plugins import json_export
 from cfmtoolbox.toolbox import CFMToolbox
 
 
 def test_plugin_can_be_loaded():
     app = CFMToolbox()
-    assert json_export_plugin in app.load_plugins()
+    assert json_export in app.load_plugins()
 
 
-def test_json_export():
-    cfm = CFM([], [], [])
-    output = export_json(cfm)
-
-    assert output.decode() == json.dumps(
-        {"features": [], "require_constraints": [], "exclude_constraints": []}, indent=2
+def test_export_json():
+    root = Feature(
+        name="sandwich",
+        instance_cardinality=Cardinality(intervals=[]),
+        group_type_cardinality=Cardinality(intervals=[]),
+        group_instance_cardinality=Cardinality(intervals=[]),
+        parents=[],
+        children=[],
     )
+
+    cfm = CFM(
+        features=[root],
+        require_constraints=[],
+        exclude_constraints=[],
+    )
+
+    output = json_export.export_json(cfm)
+    assert output.decode() == json.dumps(
+        {
+            "root": {
+                "name": "sandwich",
+                "instance_cardinality": {"intervals": []},
+                "group_type_cardinality": {"intervals": []},
+                "group_instance_cardinality": {"intervals": []},
+                "children": [],
+            },
+            "constraints": [],
+        },
+        indent=2,
+    )
+
+
+def test_serialize_feature_can_serialize_standalone_features():
+    feature = Feature(
+        name="sandwich",
+        instance_cardinality=Cardinality(intervals=[]),
+        group_type_cardinality=Cardinality(intervals=[]),
+        group_instance_cardinality=Cardinality(intervals=[]),
+        parents=[],
+        children=[],
+    )
+
+    serialized_feature = json_export.serialize_feature(feature)
+    assert serialized_feature == {
+        "name": "sandwich",
+        "instance_cardinality": {"intervals": []},
+        "group_type_cardinality": {"intervals": []},
+        "group_instance_cardinality": {"intervals": []},
+        "children": [],
+    }
+
+
+def test_serialize_feature_can_serialize_children():
+    sandwich = Feature(
+        name="sandwich",
+        instance_cardinality=Cardinality(intervals=[]),
+        group_type_cardinality=Cardinality(intervals=[]),
+        group_instance_cardinality=Cardinality(intervals=[]),
+        parents=[],
+        children=[],
+    )
+
+    bread = Feature(
+        name="bread",
+        instance_cardinality=Cardinality(intervals=[]),
+        group_type_cardinality=Cardinality(intervals=[]),
+        group_instance_cardinality=Cardinality(intervals=[]),
+        parents=[sandwich],
+        children=[],
+    )
+
+    sandwich.children = [bread]
+
+    serialized_feature = json_export.serialize_feature(sandwich)
+    assert serialized_feature == {
+        "name": "sandwich",
+        "instance_cardinality": {"intervals": []},
+        "group_type_cardinality": {"intervals": []},
+        "group_instance_cardinality": {"intervals": []},
+        "children": [
+            {
+                "name": "bread",
+                "instance_cardinality": {"intervals": []},
+                "group_type_cardinality": {"intervals": []},
+                "group_instance_cardinality": {"intervals": []},
+                "children": [],
+            }
+        ],
+    }
+
+
+def test_serialize_cardinality_can_serialize_empty_intervals():
+    cardinality = Cardinality(intervals=[])
+    serialized_cardinality = json_export.serialize_cardinality(cardinality)
+    assert serialized_cardinality == {"intervals": []}
+
+
+def test_serialize_cardinality_can_serialize_multiple_intervals():
+    cardinality = Cardinality(
+        intervals=[
+            Interval(lower=0, upper=0),
+            Interval(lower=1, upper=2),
+            Interval(lower=2, upper=None),
+        ]
+    )
+
+    serialized_cardinality = json_export.serialize_cardinality(cardinality)
+    assert serialized_cardinality == {
+        "intervals": [
+            {"lower": 0, "upper": 0},
+            {"lower": 1, "upper": 2},
+            {"lower": 2, "upper": None},
+        ]
+    }
+
+
+def test_serialize_interval_can_serialize_bound_interval():
+    interval = Interval(lower=0, upper=0)
+    serialized_interval = json_export.serialize_interval(interval)
+    assert serialized_interval == {"lower": 0, "upper": 0}
+
+
+def test_serialize_interval_can_serialize_unbound_interval():
+    interval = Interval(lower=2, upper=None)
+    serialized_interval = json_export.serialize_interval(interval)
+    assert serialized_interval == {"lower": 2, "upper": None}
+
+
+def test_serialize_constraint():
+    feature1 = Feature(
+        name="nuggets",
+        instance_cardinality=Cardinality(intervals=[]),
+        group_type_cardinality=Cardinality(intervals=[]),
+        group_instance_cardinality=Cardinality(intervals=[]),
+        parents=[],
+        children=[],
+    )
+
+    feature2 = Feature(
+        name="sauce",
+        instance_cardinality=Cardinality(intervals=[]),
+        group_type_cardinality=Cardinality(intervals=[]),
+        group_instance_cardinality=Cardinality(intervals=[]),
+        parents=[],
+        children=[],
+    )
+
+    constraint = json_export.serialize_constraint(
+        Constraint(
+            require=True,
+            first_feature=feature1,
+            first_cardinality=Cardinality(intervals=[]),
+            second_feature=feature2,
+            second_cardinality=Cardinality(intervals=[]),
+        )
+    )
+
+    assert constraint == {
+        "require": True,
+        "first_feature_name": "nuggets",
+        "first_cardinality": {"intervals": []},
+        "second_feature_name": "sauce",
+        "second_cardinality": {"intervals": []},
+    }
