8768aba372e3e4a35e7de94d145c27c50b5b847f
Jonathan Ehwald <github@ehwald.info>
2024-09-19 14:54:39 +0200

    Test CLI invokation from shell

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,7 @@
+import shutil
+import subprocess
+
+import pytest
 from typer.testing import CliRunner
 
 from cfmtoolbox import app
@@ -10,3 +14,11 @@ def test_cli_aborts_when_commands_are_invoked_without_import_option():
     assert result.exit_code == 1
     assert result.stderr == "Please provide a model via the --import option.\n"
     assert not result.stdout
+
+
+@pytest.mark.skipif(shutil.which("python3") is None, reason="python3 command required")
+def test_cli_invokation_from_shell():
+    proc = subprocess.run(["python3", "cfmtoolbox", "--help"], capture_output=True)
+    assert proc.returncode == 0
+    assert not proc.stderr
+    assert "cfmtoolbox [OPTIONS] COMMAND [ARGS]" in proc.stdout.decode()
