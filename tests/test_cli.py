a5f886c6cc01d5604e8d2eea729e16797e8e97fb
Jonathan Ehwald <github@ehwald.info>
2024-10-23 15:07:20 +0200

    Ensure the cli test always runs inside the venv

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -16,9 +16,11 @@ def test_cli_aborts_when_commands_are_invoked_without_import_option():
     assert not result.stdout
 
 
-@pytest.mark.skipif(shutil.which("python3") is None, reason="python3 command required")
+@pytest.mark.skipif(shutil.which("poetry") is None, reason="poetry command required")
 def test_cli_invokation_from_shell():
-    proc = subprocess.run(["python3", "cfmtoolbox", "--help"], capture_output=True)
+    proc = subprocess.run(
+        ["poetry", "run", "cfmtoolbox", "--help"], capture_output=True
+    )
     assert proc.returncode == 0
     assert not proc.stderr
     assert "cfmtoolbox [OPTIONS] COMMAND [ARGS]" in proc.stdout.decode()
