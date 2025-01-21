0b46b01e5d3bb68228a948af3363a93b793af9f4
kiliaaaaan <kilian.tran@gmail.com>
2025-01-21 16:05:47 +0100

    visualize cfm and export to png and svg

---
+++ cfmtoolbox/plugins/svg_export.py
@@ -0,0 +1,22 @@
+import networkx as nx
+from cfmtoolbox import CFM, Cardinality, Constraint, Feature, Interval, app
+from cfmtoolbox.plugins.reingold_tilford import GraphLayout
+
+@app.exporter(".svg")
+def export_jpg(cfm: CFM) -> bytes:
+    root = cfm.root
+    layout = create_tree(cfm, root)
+    layout.set_initial_pos()
+    layout.compute_shift(root.name)
+    layout.compute_x(root.name)
+    # layout.shift_when_overlap()
+    # layout.compute_x(root.name)
+    # layout.set_final_y()
+    return layout.display_graph(format='svg', cfm=cfm)
+
+def create_tree(cfm: CFM, root: Feature) -> GraphLayout:
+    G = nx.DiGraph()
+    for feature in cfm.features:
+        for child in feature.children:
+            G.add_edge(feature.name, child.name)
+    return GraphLayout(G, root.name)
